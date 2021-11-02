4690f7c15bf7b0ae0a212cccdb6c019540185559
Sonicbasedrop <ivan.ulysses@hotmail.com>
2021-11-02 22:53:49 +0000

    Create get_sales_data function to get data string from user

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    print(f"The data provided is {data_str} ")
 
 
 get_sales_data()
